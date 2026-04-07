03b9189b2aaac80ea26d3d681cd572697a1e4462
Phan Cao Minh Hiếu <hieupcm03@gmail.com>
2026-04-07 10:34:23 +0700

    Feature/auth UI (#81)
    * feat: initial commit
    
    * feat: Implement Sign-in/Sign-up UI

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ChatApp from "./pages/ChatApp";
+import { Toaster } from "sonner";
+
+function App() {
+  return (
+    <>
+      <Toaster richColors />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
